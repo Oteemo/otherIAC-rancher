a97e80505f47535e8a0bd5aea2daeafe68fbeae4
sjairam <sjairam@oteemo.com>
2025-01-30 15:00:43 -0600

    NESSUS_KEY replaced - 30th Jan

--- terraform-modules/file-server/templates/userdata_fileserver_ubuntu.sh
+++ terraform-modules/file-server/templates/userdata_fileserver_ubuntu.sh
@@ -13,7 +13,7 @@ if test -f "$FILE"; then
 fi
 
 /sbin/service nessusagent start
-/opt/nessus_agent/sbin/nessuscli agent link --key=c5594d6b00c83d4311632819c1671562efb1c20dbf56e261b389bb620c40aeef --host=ns-manager.itsec.harvard.edu --port=8834 --groups=LTS-AWS-Linux --name=$(hostname)
+/opt/nessus_agent/sbin/nessuscli agent link --key=NESSUS_KEY --host=ns-manager.itsec.harvard.edu --port=8834 --groups=LTS-AWS-Linux --name=$(hostname)
 
 useradd github_runner
 
