5310ed4c742200d5a7cce39050b639816d2cdcb6
sjairam <sjairam@oteemo.com>
2025-01-30 15:45:56 -0600

    Various updates - variables

--- infrastructure/sandbox/variables.tf
+++ infrastructure/sandbox/variables.tf
@@ -1,3 +1,8 @@
+variable "istio_version" {
+  description = "Istio version for Service Mesh "
+  type        = string
+}
+
 variable "rancher_secret_name" {
   default = "sandbox1_rancher_secret_name"
 }
