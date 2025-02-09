ad44405325e49e5f3c5cd2657879f01215e83b92
oteemosanjay <139782151+OteemoSanjay@users.noreply.github.com>
2025-02-09 16:22:51 -0600

    Update variables.tf

--- infrastructure/sandbox/variables.tf
+++ infrastructure/sandbox/variables.tf
@@ -1,8 +1,49 @@
+variable "certmanager_version" {
+  description = "Cert Manager version"
+  type        = string
+}
+
+variable "hostname" {
+  description = "Cluster hostname"
+  type        = string
+}
+
 variable "istio_version" {
   description = "Istio version for Service Mesh "
   type        = string
 }
 
+variable "rancher_version" {
+  description = "Rancher version"
+  type        = string
+}
+
+variable "rancher_backup_version" {
+  description = "Rancher (packaged) Backup version"
+  type        = string
+}
+
+variable "rancher_monitoring_version" {
+  description = "Rancher (packaged) Monitoring version"
+  type        = string
+}
+
+variable "rke_version" {
+  description = "RKE version"
+  #Equivalent to K8 version
+  type        = string
+}
+
 variable "rancher_secret_name" {
   default = "sandbox1_rancher_secret_name"
+}
+
+variable server_instance_type {
+  description = "AWS instance type for Server/Control nodes"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC id for or per environment"
+  type = string
 }
